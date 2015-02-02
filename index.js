6a9725fb8e7502dbc765bc77fc4864bde4653b0e
Joshua Curl <curljosh@msu.edu>
2015-02-01 23:44:31 -0500

    Callback for scraping

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ var request = require("request")
 
 var url = "https://mlh.io/seasons/s2015/events"
 
-request(url, function(err, resp, body) {
-  if (err) {
-    throw err
-  }
+var getHackathonData = function(fn) {
+  request(url, function(err, resp, body) {
+    if (err) {
+      throw err
+    }
 
-  $ = cheerio.load(body)
+    $ = cheerio.load(body)
 
-  var events = []
+    var events = []
 
-  $(".event-wrapper" ).each(function(index) {
-    var event = {}
+    $(".event-wrapper" ).each(function(index) {
+      var event = {}
 
-    event.name = $(this).find("h3").text()
-    event.date = $(this).find("p").first().text()
-    event.location = $(this).find("p").last().text()
+      event.name = $(this).find("h3").text()
+      event.date = $(this).find("p").first().text()
+      event.location = $(this).find("p").last().text()
 
-    events.push(event)
+      events.push(event)
+    })
+
+    fn(events)
   })
-})
+}
