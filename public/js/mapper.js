ce1dd7a3ee323af9732ae7fe2e266304131463d2
Dawson Botsford <DawsonBotsford@gmail.com>
2017-03-26 02:38:35 -0700

    mad cleanup

--- public/js/mapper.js
+++ public/js/mapper.js
@@ -1,57 +1,51 @@
+'use strict';
 var map;
-var url = "https://mlh.io/seasons/na-2017/events"
 
-var scrape = function(fn) {
-  $.getJSON("data", function(data) {
-    fn(data)
-  })
-}
+// eslint-disable-next-line no-unused-vars
+function fillMap() {
+  var mapOptions = {
+    zoom: 5,
+    center: new google.maps.LatLng(38, -98)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
 
-function initialize() {
-
-  scrape(function(data) {
-
-    var mapOptions = {
-      zoom:5,
-      center: new google.maps.LatLng(38, -98)
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-
-    data.forEach(function (event) {
-      var baseUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="
-      $.getJSON(baseUrl + event.location, function (data) {
-        console.log('data.results: ', data.results);
-        var point;
-        try {
-          point = data.results[0].geometry.location;
-          var info = '<p>' + event.name + '<br>' + event.location + '<br>' +
-                    event.date + '</p>'
-
-          var infowindow = new google.maps.InfoWindow({
-              content: info
+  var baseUri = 'http://localhost:3000';
+  // var baseUri = 'https://mlh-events-ofmvuznqxp.now.sh';
+  var apiUrl = baseUri + '/na-2017';
+  fetch(apiUrl)
+    .then(function fetchEvents(res) {
+      return res.json();
+    })
+    .then(function handleEvents(apiData) {
+      apiData.forEach(function createMapPoint(event) {
+        var baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+        fetch(baseUrl + event.location)
+          .then(function returnGoogleText(res) {
+            return res.json();
           })
+          .then(function handleGoogleResponse(data) {
+            // TODO: make api wrapper to return error if google cannot find location
+            var point = data.results[0].geometry.location;
+            var info = '<p>' + event.name + '<br>' + event.location + '<br>' +
+              event.date + '</p>';
 
-          var marker = new google.maps.Marker({
+            var infowindow = new google.maps.InfoWindow({
+              content: info
+            });
+
+            var marker = new google.maps.Marker({
               position: point,
               map: map,
               title: event.name
-          })
+            });
 
-          // only if point exists
-          google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-          })
-        } catch (err) {
-          // data.results[0] does not exist
-          console.error(data.results + " invalid");
-        }
+            // only if point exists
+            google.maps.event.addListener(marker, 'click', function openDetails() {
+              infowindow.open(map, marker);
+            });
+          });
 
-      })
-    })
-
-  })
+      });
+    });
 }
-//end of initialize
-
-google.maps.event.addDomListener(window, 'load', initialize);
