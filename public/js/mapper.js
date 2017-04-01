995d7d47dc7f4d221b1415d0ebd13172c331aeca
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2017-04-01 16:34:07 -0700

    handlebars map init

--- public/js/mapper.js
+++ public/js/mapper.js
@@ -1,51 +1,48 @@
 'use strict';
-var map;
-
 // eslint-disable-next-line no-unused-vars
-function fillMap() {
+function initMap() {
   var mapOptions = {
     zoom: 5,
     center: new google.maps.LatLng(38, -98)
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
+  var map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
 
-  var baseUri = 'http://localhost:3000';
-  // var baseUri = 'https://mlh-events-ofmvuznqxp.now.sh';
+  var baseUri = 'https://mlh-events.now.sh';
   var apiUrl = baseUri + '/na-2017';
-  fetch(apiUrl)
-    .then(function fetchEvents(res) {
-      return res.json();
-    })
-    .then(function handleEvents(apiData) {
-      apiData.forEach(function createMapPoint(event) {
-        var baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-        fetch(baseUrl + event.location)
-          .then(function returnGoogleText(res) {
-            return res.json();
-          })
-          .then(function handleGoogleResponse(data) {
-            // TODO: make api wrapper to return error if google cannot find location
-            var point = data.results[0].geometry.location;
-            var info = '<p>' + event.name + '<br>' + event.location + '<br>' +
-              event.date + '</p>';
+  // fetch(apiUrl)
+  //   .then(function fetchEvents(res) {
+  //     return res.json();
+  //   })
+  //   .then(function handleEvents(apiData) {
+  //     apiData.forEach(function createMapPoint(event) {
+  //       var baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+  //       fetch(baseUrl + event.location)
+  //         .then(function returnGoogleText(res) {
+  //           return res.json();
+  //         })
+  //         .then(function handleGoogleResponse(data) {
+  //           // TODO: make api wrapper to return error if google cannot find location
+  //           var point = data.results[0].geometry.location;
+  //           var info = '<p>' + event.name + '<br>' + event.location + '<br>' +
+  //             event.date + '</p>';
 
-            var infowindow = new google.maps.InfoWindow({
-              content: info
-            });
+  //           var infowindow = new google.maps.InfoWindow({
+  //             content: info
+  //           });
 
-            var marker = new google.maps.Marker({
-              position: point,
-              map: map,
-              title: event.name
-            });
+  //           var marker = new google.maps.Marker({
+  //             position: point,
+  //             map: map,
+  //             title: event.name
+  //           });
 
-            // only if point exists
-            google.maps.event.addListener(marker, 'click', function openDetails() {
-              infowindow.open(map, marker);
-            });
-          });
+  //           // only if point exists
+  //           google.maps.event.addListener(marker, 'click', function openDetails() {
+  //             infowindow.open(map, marker);
+  //           });
+  //         });
 
-      });
-    });
+  //     });
+  //   });
 }
