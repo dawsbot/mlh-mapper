e0af0b8ba720baf2fcd9c53f30092365a4aa1f17
Dawson Botsford <DawsonBotsford@gmail.com>
2017-04-02 13:11:58 -0700

    functional
    * href to hackathon url on marker info section
    * Resize map to be not fullscreen

--- public/js/mapper.js
+++ public/js/mapper.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 function initMap() {
   var mapOptions = {
-    zoom: 5,
+    zoom: 4,
     center: new google.maps.LatLng(38, -98)
   };
   var map = new google.maps.Map(document.getElementById('map'),
@@ -30,7 +30,7 @@ function initMap() {
             }
             // TODO: make api wrapper to return error if google cannot find location
             var point = data.results[0].geometry.location;
-            var info = '<p>' + event.name + '<br>' + event.location + '<br>' +
+            var info = '<p><a href="' + event.url + '">' + event.name + '</a><br>' + event.location + '<br>' +
               event.date + '</p>';
 
             var infowindow = new google.maps.InfoWindow({
